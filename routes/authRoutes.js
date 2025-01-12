6f3f24cea99413d72b4dc4c3e93ab8b65acbae40
servetisikli <servetisikli@gmail.com>
2025-01-12 01:53:56 +0100

    user orders / guets orders

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import { registerUser, loginUser, getMe } from "../controllers/authController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser); // Register endpoint
 router.post("/login", loginUser); // Login endpoint
+router.get("/me", protect, getMe); // Get current user endpoint
 
 export default router;
-
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createOrder, getOrders, getOrder, updateOrderStatus } from '../controllers/orderController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/orders').post(createOrder).get(getOrders);
-router.route('/orders/:id').get(getOrder).put(updateOrderStatus);
+router.route('/orders').post(createOrder).get(protect, getOrders);
+router.route('/orders/:id').get(protect, getOrder).put(protect, updateOrderStatus);
 
 export default router;
