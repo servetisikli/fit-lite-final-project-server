d6e683413a6951b3955935b52bbccdd3918d2b43
servetisikli <servetisikli@gmail.com>
2025-01-14 12:50:30 +0100

    edited

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
