8b8855e26617050650565d73efd62a61f32d6db1
Servet Isikli <servetisikli@gmail.com>
2025-01-21 16:51:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(morgan("dev"));
 
 // Enable CORS (Cross-Origin Resource Sharing)
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://fitlite.netlify.app",
 };
 app.use(cors(corsOptions));
 
